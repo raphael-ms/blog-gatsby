133a118ed387759e1094a7ac8e7f85773f1f8a89
Raphael Marques <raphaelmarques77@gmail.com>
2022-03-28 19:41:20 +0100

    add strapi configuration @ gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,55 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-postcss",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-strapi",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        apiURL: process.env.STRAPI_API_URL || "http://localhost:4200",
+        //accessToken: process.env.STRAPI_TOKEN,
+        collectionTypes: [
+          {
+            singularName: "category",
+          },
+          {
+            singularName: "post",
+          },
+          {
+            singularName: "user",
+          },
+        ],
+        // singleTypes: [
+        //   {
+        //     singularName: "about",
+        //     queryParams: {
+        //       populate: {
+        //         blocks: {
+        //           populate: "*",
+        //         },
+        //       },
+        //     },
+        //   },
+        //   {
+        //     singularName: "global",
+        //     queryParams: {
+        //       populate: {
+        //         favicon: "*",
+        //         defaultSeo: {
+        //           populate: "*",
+        //         },
+        //       },
+        //     },
+        //   },
+        // ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-transformer-remark",
   ],
 }
