9859a9bf3e3f309aa83127a6b577a526399a7bc9
Raphael Marques <raphaelmarques77@gmail.com>
2022-03-29 19:28:35 +0100

    Sort dynamic blocks create new page to the posts

---
+++ src/pages/posts.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Layout from "../components/layout"
+import ArticlesGrid from "../components/articles-grid"
+import Seo from "../components/seo"
+import Headings from "../components/headings"
+
+const IndexPage = () => {
+  const { allStrapiPost, strapiGlobal } = useStaticQuery(graphql`
+    query {
+      allStrapiPost {
+        nodes {
+          ...ArticleCard
+        }
+      }
+      strapiGlobal {
+        SiteName
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <Seo seo={{ metaTitle: "Blog-Posts" }} />
+      <Headings title={strapiGlobal.SiteName} />
+      <main>
+        <ArticlesGrid articles={allStrapiPost.nodes} />
+      </main>
+    </Layout>
+  )
+}
+
+export default IndexPage
