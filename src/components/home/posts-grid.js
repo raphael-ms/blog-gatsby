7c7714d0bfa4d86a5b2d7f5f7b8d0e313da38f72
Raphael Marques <raphaelmarques77@gmail.com>
2022-03-30 21:15:20 +0100

    fix "Ver mais" button

--- src/components/home/posts-grid.js
+++ src/components/home/posts-grid.js
@@ -6,14 +6,14 @@ const PostsGrid = ({ posts }) => {
   const { nodes } = posts
   console.log(nodes)
   return (
-    <div className="container mt-12">
+    <section className="container mt-12">
       <h1 className="font-sans text-3xl text-purple-700 font-bold">POSTS</h1>
       <div className="mt-12 grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {nodes.map((post, idx) => (
           <PostCard key={idx} index={idx} post={post} />
         ))}
       </div>
-      <div className="mt-10">
+      <div className="flex justify-center mt-10">
         <Link
           className="flex items-center justify-center w-52 text-center h-10 bg-purple-700 text-white self-center rounded-full cursor-pointer hover:shadow-lg hover:bg-purple-600"
           to={`/posts`}
@@ -21,7 +21,7 @@ const PostsGrid = ({ posts }) => {
           VER MAIS
         </Link>
       </div>
-    </div>
+    </section>
   )
 }
 
